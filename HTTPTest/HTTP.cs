bd08810dbce64673ff4084669b3c0449b42275a7
Brad <bjkapteyn@gmail.com>
2019-12-30 11:51:38 -0500

    first

---
+++ HTTPTest/HTTP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HTTPTest
+{
+    public class HTTPServer
+    {
+        private bool Running = false;
+
+        private TcpListener listener;
+
+        public HTTPServer(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+
+        }
+
+        public void Start()
+        {
+            Thread serverThread = new Thread(new ThreadStart(Run));
+            serverThread.Start();
+        }
+
+        private void Run()
+        {
+            Running = true;
+            listener.Start();
+
+            while(Running)
+            {
+
+            }
+        }
+    }
+}
