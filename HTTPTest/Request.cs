ad11a51f70d0e18ec79f036744e2c5a2dd7bc0b3
Brad <bjkapteyn@gmail.com>
2020-01-08 13:48:05 -0500

    Debug, changed url assignment to correct index in GetRequest method

--- HTTPTest/Request.cs
+++ HTTPTest/Request.cs
@@ -11,6 +11,7 @@ namespace HTTPTest
         public string Type { get; set; }
         public string URL { get; set; }
         public string Host { get; set; }
+        public string Referrer { get; set; }
 
         private Request(string type, string url, string host)
         {
@@ -29,8 +30,18 @@ namespace HTTPTest
             //parse the request string and grab the type url and host
             string[] tokens = request.Split(' ');
             string type = tokens[0];
-            string url = tokens[2];
+            string url = tokens[1];
             string host = tokens[4];
+            string referrer = "";
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if(tokens[i] == "Referrer:")
+                {
+                    referrer = tokens[i + 1];
+                    break;
+                }
+            }
             return new Request(type, url, host);
         }
     }
